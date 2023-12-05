61990745757158e05da4f7a2396d980bbc649756
zeeshantaj <zeeshantaj.zt@gmail.com>
2023-12-05 23:45:01 +0500

    first1 commit

---
+++ app/src/main/java/com/example/movie_booking_application/Activity/Booking_Activity.java
@@ -0,0 +1,17 @@
+package com.example.movie_booking_application.Activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.movie_booking_application.R;
+
+public class Booking_Activity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+    }
+}
