b9c911e159c376544f28eebd23687fa8c7590188
zeeshantaj <zeeshantaj.zt@gmail.com>
2023-12-06 18:47:44 +0500

    first4 commit

--- app/src/main/java/com/example/movie_booking_application/Activity/Booking_Activity.java
+++ app/src/main/java/com/example/movie_booking_application/Activity/Booking_Activity.java
@@ -1,7 +1,8 @@
 package com.example.movie_booking_application.Activity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +10,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.movie_booking_application.R;
 
 public class Booking_Activity extends AppCompatActivity {
+    private ImageView add,minus;
+    private TextView incrementText;
+    private int count = 1;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        add = findViewById(R.id.addBtn);
+        minus = findViewById(R.id.minusBtn);
+        incrementText = findViewById(R.id.incrementTxt);
+
+        add.setOnClickListener(v -> {
+            count++;
+            incrementText.setText(String.valueOf(count));
+        });
+        minus.setOnClickListener(v -> {
+            if (count > 1){
+                count--;
+                incrementText.setText(String.valueOf(count));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/movie_booking_application/MainActivity.java
+++ app/src/main/java/com/example/movie_booking_application/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.lifecycle.LifecycleCoroutineScope;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ClipData;
 import android.os.Bundle;
 
 import com.example.movie_booking_application.Adapter.MoviesAdapter;
 import com.example.movie_booking_application.Model.Movies;
+import com.google.android.material.carousel.CarouselStrategy;
+
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
+import org.imaginativeworld.whynotimagecarousel.listener.CarouselOnScrollListener;
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +26,14 @@ public class MainActivity extends AppCompatActivity {
     private Movies movies;
     private List<Movies> moviesList;
     private MoviesAdapter adapter;
+    private ImageCarousel carousel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.movie_recycler);
+        carousel = findViewById(R.id.carousel);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
         Movies movies1 = new Movies("the dark knoght rises", "fhaspoifhoidashfoihafsdoihdgoifayhsoifhasdpoihgfiposahdifg", "fdlkhfdajlkdhafsl");
@@ -44,6 +52,16 @@ public class MainActivity extends AppCompatActivity {
         moviesList.add(movies6);
         adapter = new MoviesAdapter(moviesList);
         recyclerView.setAdapter(adapter);
+        getSlider();
+
     }
 
+    private void getSlider(){
+        carousel.addData(new CarouselItem("https://www.yashrajfilms.com/images/default-source/movies/tiger-3/tiger-3_banner.jpg?sfvrsn=c63bdfcc_0"));
+        carousel.addData(new CarouselItem("https://www.yashrajfilms.com/images/default-source/default-album/tgif_main-page.jpg?sfvrsn=de26dfcc_0"));
+        carousel.addData(new CarouselItem("https://i0.wp.com/3.bp.blogspot.com/-AzNL9sB_8YU/U1pfKjnzUII/AAAAAAAAAMQ/cUF6BMCEwMU/s1600/edge_of_tomorrow_banner-poster+(2).jpg"));
+        carousel.addData(new CarouselItem("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVD9q011bzmUGR0-j2x06cERhZQXe_dWkLwA&usqp=CAU"));
+        carousel.addData(new CarouselItem("https://www.scifinow.co.uk/wp-content/uploads/2016/04/warcraft_ver19_xlg-616x303.jpg"));
+        carousel.addData(new CarouselItem("https://collider.com/wp-content/uploads/dark-knight-rises-movie-poster-banner-batman.jpg"));
+    }
 }
