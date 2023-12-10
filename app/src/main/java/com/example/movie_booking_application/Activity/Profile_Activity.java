2a4cc6ae73d8665f9b85896886825a1d9b1e1c4b
zeeshantaj <zeeshantaj.zt@gmail.com>
2023-12-10 15:21:00 +0500

    third commit

--- app/src/main/java/com/example/movie_booking_application/Activity/Profile_Activity.java
+++ app/src/main/java/com/example/movie_booking_application/Activity/Profile_Activity.java
@@ -4,16 +4,33 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.movie_booking_application.Adapter.TicketAdapter;
+import com.example.movie_booking_application.Model.BookingModel;
 import com.example.movie_booking_application.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kotlinx.coroutines.channels.Receive;
 
 public class Profile_Activity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private TicketAdapter adapter;
+    private List<BookingModel> ticketList;
+    private RecyclerView recyclerView;
+    private ImageView profileImage;
+    private TextView profileName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +39,21 @@ public class Profile_Activity extends AppCompatActivity {
         Intent intent = getIntent();
         String name = intent.getStringExtra("personName");
         String imageUrl = intent.getStringExtra("imageUrl");
+        profileImage = findViewById(R.id.profileImage);
+        profileName = findViewById(R.id.profileName);
+
+
+        recyclerView = findViewById(R.id.profileRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+        ticketList = new ArrayList<>();
+        adapter = new TicketAdapter(ticketList);
+
 
         setToolbar();
+    }
+    private void initRecycler(){
+
     }
     private void setToolbar(){
         toolbar = findViewById(R.id.bookedToolbar);
---
+++ app/src/main/java/com/example/movie_booking_application/Adapter/TicketAdapter.java
@@ -0,0 +1,65 @@
+package com.example.movie_booking_application.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.movie_booking_application.Activity.Booking_Activity;
+import com.example.movie_booking_application.Model.BookingModel;
+import com.example.movie_booking_application.R;
+
+import java.util.List;
+
+public class TicketAdapter extends RecyclerView.Adapter<TicketAdapter.ViewHolder> {
+
+    List<BookingModel> ticketList;
+
+    public TicketAdapter(List<BookingModel> ticketList) {
+        this.ticketList = ticketList;
+    }
+
+    @NonNull
+    @Override
+    public TicketAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ticket_view,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TicketAdapter.ViewHolder holder, int position) {
+        BookingModel model = ticketList.get(position);
+        holder.showTime.setText(model.getTiming());
+        holder.person.setText(model.getPerson());
+        holder.date.setText(model.getCurrentTime());
+        holder.title.setText(model.getMovieTitle());
+
+        Glide.with(holder.itemView.getContext())
+                .load(model.getImageUrl())
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return ticketList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView title,date,person,showTime;
+        private ImageView imageView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.ticket_movieTitle);
+            date = itemView.findViewById(R.id.ticket_date);
+            person = itemView.findViewById(R.id.ticket_person);
+            imageView = itemView.findViewById(R.id.imageView2);
+            showTime = itemView.findViewById(R.id.ticketShowTime);
+        }
+    }
+}
